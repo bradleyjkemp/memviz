2d897c35db5228990f6fd2b450f0a518aaf9076c
Bradley Kemp <bradleyjkemp@users.noreply.github.com>
2017-11-28 22:59:17 +0000

    Added graphviz record output and fixed tests (#2)

--- memmap.go
+++ memmap.go
@@ -3,58 +3,77 @@ package memmap
 import (
 	"fmt"
 	"reflect"
-	"strings"
 	"unsafe"
 
 	"io"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
-var spewer = &spew.ConfigState{
-	Indent:                  "  ",
-	SortKeys:                true, // maps should be spewed in a deterministic order
-	DisablePointerAddresses: true, // don't spew the addresses of pointers
-	DisableCapacities:       true, // don't spew capacities of collections
-	SpewKeys:                true, // if unable to sort map keys then spew keys to strings and sort those
-	MaxDepth:                1,
-}
+//var spewer = &spew.ConfigState{
+//	Indent:                  "  ",
+//	SortKeys:                true, // maps should be spewed in a deterministic order
+//	DisablePointerAddresses: true, // don't spew the addresses of pointers
+//	DisableCapacities:       true, // don't spew capacities of collections
+//	SpewKeys:                true, // if unable to sort map keys then spew keys to strings and sort those
+//	MaxDepth:                1,
+//}
 
 // Map prints out a Graphviz digraph of the given datastructure to the given io.Writer
 func Map(w io.Writer, i interface{}) {
 	iVal := reflect.ValueOf(i)
-	fmt.Fprintln(w, "digraph {")
-	imap(w, iVal)
+	fmt.Fprintln(w, "digraph structs {")
+	fmt.Fprintln(w, "  node [shape=Mrecord];")
+	imap(w, iVal, map[uintptr]int{})
 	fmt.Fprintln(w, "}")
 }
 
-func imap(w io.Writer, iVal reflect.Value) uintptr {
+func imap(w io.Writer, iVal reflect.Value, nodeIds map[uintptr]int) (uintptr, map[uintptr]int) {
 	if iVal.Kind() == reflect.Ptr || iVal.Kind() == reflect.Interface {
 		if !iVal.IsNil() {
-			return imap(w, iVal.Elem())
+			return imap(w, iVal.Elem(), nodeIds)
 		}
-		return 0
+		return 0, nodeIds
 	}
 
 	if iVal.Kind() != reflect.Struct {
-		return 0
+		return 0, nodeIds
 	}
 
 	iValAddr := iVal.UnsafeAddr()
-	nodeLabel := strings.Replace(spewer.Sdump(iVal.Interface()), "\n", "\\n", -1)
-	fmt.Fprintf(w, "  %d [label=\"%s\"];\n", iValAddr, nodeLabel)
+	if _, ok := nodeIds[iValAddr]; ok {
+		return iValAddr, nodeIds
+	}
+
+	nodeIds[iValAddr] = len(nodeIds)
+
+	var fields []string
+	var links []string
 
 	uType := iVal.Type()
-	for i := 0; i < uType.NumField(); i++ {
-		field := iVal.Field(i)
+	for index := 0; index < uType.NumField(); index++ {
+		field := iVal.Field(index)
+		field = reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem()
 		if field.Kind() == reflect.Ptr || field.Kind() == reflect.Interface {
-			field = reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem()
-			ptr := imap(w, field)
+			fields = append(fields, uType.Field(index).Name)
+			ptr, rNodeIds := imap(w, field, nodeIds)
 			if ptr != 0 {
-				fmt.Fprintf(w, "  %d -> %d;\n", iValAddr, ptr)
+				nodeIds = rNodeIds
+				links = append(links, fmt.Sprintf("  %d:f%d -> %d:name;\n", nodeIds[iValAddr], index, nodeIds[ptr]))
 			}
+		} else {
+			fields = append(fields, fmt.Sprintf("%s: %s", uType.Field(index).Name, field.Interface()))
 		}
 	}
 
-	return iValAddr
+	node := fmt.Sprintf("  %d [label=\"<name> %s", nodeIds[iValAddr], iVal.Type().Name())
+	for index, name := range fields {
+		node += fmt.Sprintf("|<f%d> %s", index, name)
+	}
+	node += "\"];\n"
+
+	fmt.Fprint(w, node)
+	for _, link := range links {
+		fmt.Fprint(w, link)
+	}
+
+	return iValAddr, nodeIds
 }
